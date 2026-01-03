b0435796a2b8ede871359473e84c34d72049eb41
Sreej <g.sreeja119@gmail.com>
2026-01-03 14:06:19 -0500

    Cool comment 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/LimelightTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/LimelightTesting.java
@@ -4,11 +4,12 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-
+@TeleOp(name = "TestLimelight")
 public class LimelightTesting extends OpMode {
     private Limelight3A limelight;
     private IMU imu;
