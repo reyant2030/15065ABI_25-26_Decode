1aa86d6a5871b684114dd74edb6e95c9da7b477f
Sreej <g.sreeja119@gmail.com>
2026-01-24 12:37:46 -0500

    ................

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/MainTeleOp.java
@@ -3,58 +3,32 @@ package org.firstinspires.ftc.teamcode.TeleOpModes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.Devices.LeftColorSensor;
-import org.firstinspires.ftc.teamcode.Devices.RightColorSensor;
-import org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain;
-import org.firstinspires.ftc.teamcode.Mechanisms.Intake;
+import org.firstinspires.ftc.teamcode.Mechanisms.Booster;
 import org.firstinspires.ftc.teamcode.Mechanisms.Outtake;
 
 @TeleOp(name = "Main TeleOp")
-public class MainTeleOp extends OpMode {
-    Drivetrain drivetrain = new Drivetrain();
-    double forward, strafe, rotate;
-
-    Intake intake = new Intake();
-    RightColorSensor rightColorSensor = new RightColorSensor();
-    LeftColorSensor leftColorSensor = new LeftColorSensor();
+public class MainTeleOp extends OpMode
+{
     Outtake outtake = new Outtake();
+    Booster booster = new Booster();
 
     @Override
-    public void init() {
-        drivetrain.initDrivetrain(hardwareMap);
-        intake.initIntake(hardwareMap);
-        rightColorSensor.initRightColorSensor(hardwareMap);
-        leftColorSensor.initRightColorSensor(hardwareMap);
+    public void init()
+    {
         outtake.initOuttake(hardwareMap);
+        booster.initBooster(hardwareMap);
     }
 
     @Override
     public void loop() {
-        forward = gamepad1.left_stick_y;
-        strafe = gamepad1.left_stick_x;
-        rotate = gamepad1.right_stick_x;
-        drivetrain.drive(forward, strafe, rotate);
-
-        if (gamepad1.right_bumper) {
-            intake.setIntakePower(1);
-        }
-        else if(gamepad1.left_bumper){
-            intake.setIntakePower(-1);
-        }
-        else{
-            intake.setIntakePower(0);
+        if (gamepad1.x)
+        {
+            outtake.setVelocity(2800);
         }
 
-        if (gamepad2.right_bumper) {
-            outtake.setVelocity(1900);
-        }
-        else if (gamepad2.left_bumper) {
-            outtake.setVelocity(1750);
-        }
-        else {
-            outtake.setVelocity(0);
+        if (gamepad1.b)
+        {
+            booster.setBoosterPower(1);
         }
     }
 }
-
-// Fate was in ABI's favor today
