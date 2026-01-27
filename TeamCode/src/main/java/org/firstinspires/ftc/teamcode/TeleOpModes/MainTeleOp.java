b8278c3dee9c1f5ffdda74ae2e935fbe5b586c7b
Reyan <reyant2030@gmail.com>
2026-01-27 16:32:11 -0500

    Transfer Code added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/MainTeleOp.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.TeleOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.Mechanisms.Booster;
@@ -35,25 +34,35 @@ public class MainTeleOp extends OpMode {
         spindexerMotor= hardwareMap.get(DcMotorEx.class, "SpindexerMotor");
         spindexerMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         spindexerMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
-        spindexerMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        spindexerMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
     public void loop() {
+
         forward = -gamepad1.right_stick_y;
         strafe = gamepad1.right_stick_x;
         rotate = gamepad1.left_stick_x;
         drivetrain.drive(forward, strafe, rotate);
 
         if (gamepad1.right_bumper) {
-            intake.setIntakePower(0.54260869565);
+            intake.setIntakePower(1);
+            booster.setBoosterPower(0.5);
         }
         else {
             intake.setIntakePower(0);
+            booster.setBoosterPower(0);
         }
 
         if (gamepad1.dpad_up) {
-            encoder(3);
+            encoder();
+        }
+
+        if (gamepad1.x) {
+            transfer.setTransferPosition(0.8);
+        }
+        else {
+            transfer.setTransferPosition(0);
         }
 
         if (gamepad1.a) {
@@ -71,11 +80,12 @@ public class MainTeleOp extends OpMode {
         }
     }
 
-    public void encoder(double divisor) {
-        target = ticks/divisor;
+    public void encoder() {
+        spindexerMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        target += 48.3666666667;
         spindexerMotor.setTargetPosition((int)target);
-        spindexerMotor.setTargetPositionTolerance(10);
-        spindexerMotor.setPower(1);
+        spindexerMotor.setTargetPositionTolerance(5);
+        spindexerMotor.setPower(0.5);
         spindexerMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
     }
 }
